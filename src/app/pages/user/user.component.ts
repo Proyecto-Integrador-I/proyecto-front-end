fe240e2dd4f1a07ec1aad3a3fcd86d4a3b4bf231
Steven <steven.deibi@hotmail.com>
2020-05-14 15:31:12 -0500

    HU-01 Seteo automático de campos en el formulario registrar actividad

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -16,7 +16,7 @@ export class UserComponent implements OnInit {
   actividad: Actividad = new Actividad();
   resultadoCursos: Curso[];
 
-  constructor(private router:Router, private service:ActividadService, private serviceCurso: CursoService) {}
+  constructor(private router:Router, private serviceActividad:ActividadService, private serviceCurso: CursoService) {}
 
   public isError = false;
 
@@ -34,33 +34,24 @@ export class UserComponent implements OnInit {
           }
         );
     }else{
+      /*
       $('select').material_select('destroy');
       this.serviceCurso.getCursosId(id)
         .subscribe(data =>{
             this.resultadoCursos = data;
           }
         );
+       */
+      this.serviceActividad.getActividadId(+id)
+        .subscribe(data =>{
+          this.actividad = data[0];
+        })
     }
-
-    /*
-    this.serviceActividad.getActividadId(+id)
-      .subscribe(data =>{
-        this.actividad = data[0];
-        arregloDeSubCadenas = this.actividad.fecha.split('/')
-        dia = arregloDeSubCadenas[0];
-        mes = arregloDeSubCadenas[1];
-        if(mes < 10){
-          mes = '0'+ mes;
-        }
-        anio = arregloDeSubCadenas[2];
-        this.fecha = anio + '-' + mes + '-' + dia;
-      })
-     */
   }
 
   onRegistrar(form: NgForm): void{
     if(form.valid){
-      this.service.registrarActividad(this.actividad)
+      this.serviceActividad.registrarActividad(this.actividad)
         .subscribe(data=>{
           alert('¡ Actividad registrada con éxito !');
           this.router.navigate(['actividades']);

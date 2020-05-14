b3a99d9bab2a8633ea54375ddad82d1454bee917
Steven <steven.deibi@hotmail.com>
2020-05-14 15:17:41 -0500

    HU-01: Despliegue de lista de cursos en el formulario registrar actividad

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -3,7 +3,10 @@ import {NgForm} from '@angular/forms';
 import {Router} from "@angular/router";
 import {ActividadService} from "../../services/actividad.service";
 import {Actividad} from '../../model/Actividad';
+import {CursoService} from "../../services/curso.service";
+import {Curso} from "../../model/Curso";
 
+declare var $:any;
 
 @Component({
   selector: 'app-user',
@@ -11,12 +14,49 @@ import {Actividad} from '../../model/Actividad';
 })
 export class UserComponent implements OnInit {
   actividad: Actividad = new Actividad();
+  resultadoCursos: Curso[];
 
-  constructor(private router:Router, private service:ActividadService) {}
+  constructor(private router:Router, private service:ActividadService, private serviceCurso: CursoService) {}
 
   public isError = false;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setearCampos();
+  }
+  setearCampos(){
+    const id = localStorage.getItem('idCurso');
+    console.log(id);
+    if(id === '0'){
+      $('select').material_select('destroy');
+      this.serviceCurso.getCursos()
+        .subscribe(data =>{
+            this.resultadoCursos = data;
+          }
+        );
+    }else{
+      $('select').material_select('destroy');
+      this.serviceCurso.getCursosId(id)
+        .subscribe(data =>{
+            this.resultadoCursos = data;
+          }
+        );
+    }
+
+    /*
+    this.serviceActividad.getActividadId(+id)
+      .subscribe(data =>{
+        this.actividad = data[0];
+        arregloDeSubCadenas = this.actividad.fecha.split('/')
+        dia = arregloDeSubCadenas[0];
+        mes = arregloDeSubCadenas[1];
+        if(mes < 10){
+          mes = '0'+ mes;
+        }
+        anio = arregloDeSubCadenas[2];
+        this.fecha = anio + '-' + mes + '-' + dia;
+      })
+     */
+  }
 
   onRegistrar(form: NgForm): void{
     if(form.valid){
